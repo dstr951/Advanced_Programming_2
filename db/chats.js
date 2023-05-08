154a801c51f6440c0b690b77943314fe162bb900
dstr951 <dstr159@gmail.com>
2023-05-08 23:51:12 +0300

    add messages to DB

---
+++ db/chats.js
@@ -0,0 +1,366 @@
+const chat1 = {
+    chatId: 1,
+    messages: [
+        {
+            content:"message1",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 2,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 2,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 2,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat2 = {
+    chatId: 2,
+    messages: [
+        {
+            content:"message1",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 3,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 3,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 1,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat3 = {
+    chatId: 3,
+    messages: [
+        {
+            content:"message1",
+            senderId: 4,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 4,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 1,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat4 = {
+    chatId: 4,
+    messages: [
+        {
+            content:"message1",
+            senderId: 5,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 1,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat5 = {
+    chatId: 5,
+    messages: [
+        {
+            content:"message1",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 6,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 6,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 6,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat6 = {
+    chatId: 6,
+    messages: [
+        {
+            content:"message1",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 7,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 7,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat7 = {
+    chatId: 7,
+    messages: [
+        {
+            content:"message1",
+            senderId: 8,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 8,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 1,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat8 = {
+    chatId: 8,
+    messages: [
+        {
+            content:"message1",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 9,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 9,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat9 = {
+    chatId: 9,
+    messages: [
+        {
+            content:"message1",
+            senderId: 10,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 10,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 10,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 10,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 10,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat10 = {
+    chatId: 10,
+    messages: [
+        {
+            content:"message1",
+            senderId: 11,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 11,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 1,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat11 = {
+    chatId: 1,
+    messages: [
+        {
+            content:"message1",
+            senderId: 12,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 12,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 12,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 12,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 12,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+const chat12 = {
+    chatId: 1,
+    messages: [
+        {
+            content:"message1",
+            senderId: 13,
+            timeSent: new Date("2023-04-17T03:24:00")
+        },
+        {
+            content:"message2",
+            senderId: 13,
+            timeSent: new Date("2023-04-17T08:00:00")
+        },
+        {
+            content:"message3",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:02:00")
+        },
+        {
+            content:"message4",
+            senderId: 1,
+            timeSent: new Date("2023-04-17T08:04:00")
+        },
+        {
+            content:"message5",
+            senderId: 13,
+            timeSent: new Date("2023-04-18T08:02:00")
+        },
+    ],
+}
+
+export const chats = [
+    chat1, chat2, chat3, chat4,
+    chat5, chat6, chat7, chat8,
+    chat9, chat10, chat11, chat12
+]
