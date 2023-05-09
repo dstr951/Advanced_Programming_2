9138843444a812363b6ed3bda1718d3b5b5741ea
dstr951 <dstr159@gmail.com>
2023-05-10 02:30:29 +0300

    add Message component

---
+++ client-side/src/components/chat/Message.js
@@ -0,0 +1,12 @@
+export default function Message(){
+	return(
+		<div class="row m-0">
+          <div class="message col-12">
+            <div class="message-in">
+              <span class="message-text">בדיקה של הודעה</span>
+              <span class="message-time">4:20</span>
+            </div>
+          </div>
+        </div>
+	)
+}
--- client-side/src/components/chat/OpenChatMessages.js
+++ client-side/src/components/chat/OpenChatMessages.js
@@ -1,92 +1,10 @@
+import Message from "./Message"
 export default function OpenChatMessages() {
   return (
     <div id="message_box" class="row">
       <div class="messages-buffer"></div>
       <div class="messages">
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-        </div>
-        <div class="row m-0">
-          <div class="message col-12">
-            <div class="message-out">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
-            </div>
-          </div>
-          <div class="row m-0">
-            <div class="message col-12">
-              <div class="message-out">
-                <span class="message-text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  enim nisi, euismod eget elit et, rutrum placerat nunc.
-                  Vestibulum non ligula ex. Donec congue iaculis consequat.
-                  Etiam dui risus, semper vel feugiat eu, porttitor in tortor.
-                  Pellentesque vulputate mi velit, eu mollis libero pharetra
-                  vel. Duis vitae massa nec sapien fringilla ornare. Integer sed
-                  massa nec nunc condimentum lacinia. Aenean malesuada nisi nec
-                  sem fringilla tincidunt. Aenean volutpat elementum ante,
-                  facilisis ultrices tortor semper sed. Integer sit amet purus
-                  vel dui luctus suscipit. In rhoncus finibus luctus. Nulla nec
-                  eros eget tellus blandit sollicitudin quis at nulla.
-                </span>
-                <span class="message-time">22:56</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Message />        
       </div>
     </div>
   );
