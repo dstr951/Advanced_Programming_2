f86e871a6489dd8db7025cfe85481c3ab08788b1
dstr951 <dstr159@gmail.com>
2023-05-10 02:48:07 +0300

    add parameters to Message component

--- client-side/src/components/chat/Message.js
+++ client-side/src/components/chat/Message.js
@@ -1,10 +1,12 @@
-export default function Message(){
+export default function Message({message, myId}){
+	const {content, timeSent, senderId} = message
+	const messageClass = senderId === myId ? "message-out" : "message-in"
 	return(
 		<div class="row m-0">
           <div class="message col-12">
-            <div class="message-in">
-              <span class="message-text">בדיקה של הודעה</span>
-              <span class="message-time">4:20</span>
+            <div class={messageClass}>
+              <span class="message-text">{content}</span>
+              <span class="message-time">{timeSent.getHours()}:{timeSent.getMinutes()}</span>
             </div>
           </div>
         </div>
