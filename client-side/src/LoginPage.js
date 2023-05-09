36d20f5842694629efe9e7145e9b4779707d2c99
tomerKimberg <tomerkimberg@gmail.com>
2023-05-09 19:59:58 +0300

    added login validation

--- client-side/src/LoginPage.js
+++ client-side/src/LoginPage.js
@@ -13,8 +13,8 @@ function LoginPage(){
     const buildResponse = function (e,userName,password){
         //stop the form default response
         e.preventDefault();
-        if(validateLogin(userName,password) === 1){
-
+        if(validateLogin(userName,password).code  === 200){
+            setDisplayError(false)
         }
         else{
             setDisplayError(true)
@@ -62,9 +62,7 @@ function LoginPage(){
                     </form>
                 </div>
             </div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-                    crossOrigin="anonymous"></script>
+
         </div>
 
     )
