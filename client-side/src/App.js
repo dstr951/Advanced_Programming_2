917ded61288f9ff99bdb65f43d4560d75349943b
dstr951 <dstr159@gmail.com>
2023-05-10 06:40:16 +0300

    use app to pass parameters to other components

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./chats.css";
-import UserChatRow from "./components/chat/UserChatRow.js"
-import ChatRow from "./components/chat/ChatRow.js"
-import OpenChat from "./components/chat/OpenChat.js"
+import UserChatRow from "./components/chat/UserChatRow.js";
+import ChatRow from "./components/chat/ChatRow.js";
+import OpenChat from "./components/chat/OpenChat.js";
+import { getAllChats } from "./apiTemp";
 
 function App() {
+  /*TEMP*/
+  const [myId, setMyId] = useState(0);
+  setTimeout(() => {
+    setMyId(1);
+  }, 100);
+  /*TEMP*/
+  const [chatUsers, setChatUsers] = useState([]);
+  //hook to get the chats data from the server
+  useEffect(() => {
+    const response = getAllChats(myId);
+    if (response.code === 200) {
+      setChatUsers(response.body);
+    }
+  }, [myId]);
+  //control the content displayed on screen
+  const [openChatId, setOpenChatId] = useState(0);
+  const [openUser, setOpenUser] = useState({});
+
   return (
     <>
       <div className="background-jumbo"></div>
@@ -16,13 +36,22 @@ function App() {
         <div id="chat_card" className="col-12">
           <div id="panels_row" className="row m-0">
             <div id="conversations_panel" className=" col-5 chat-panel">
-              <UserChatRow />
+              <UserChatRow myId={1} />
               <div id="conversations">
-                <ChatRow />
+                {chatUsers.map((cu, index) => (
+                  <ChatRow
+                    userId={cu.userId}
+                    chatId={cu.chatId}
+                    changeOpenChatId={setOpenChatId}
+                    changeOpenUser={setOpenUser}
+                    active={openUser.userId === cu.userId}
+                    key={index}
+                  />
+                ))}
               </div>
             </div>
             <div id="messages_panel" className=" col-7 chat-panel">
-              <OpenChat />
+              <OpenChat user={openUser} chatId={openChatId} myId={myId} />
             </div>
           </div>
         </div>
