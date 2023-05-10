fe0f23e1f99d882af5c3537ffe56d7c9df0f8986
dstr951 <dstr159@gmail.com>
2023-05-10 03:35:01 +0300

    add parameters to OpenChatContactRow

--- client-side/src/components/chat/OpenChatContactRow.js
+++ client-side/src/components/chat/OpenChatContactRow.js
@@ -1,15 +1,25 @@
-export default function OpenChatContactRow() {
+import { useEffect, useState } from 'react'
+import { getUserStatus } from "../../apiTemp";
+
+export default function OpenChatContactRow({ user }) {
+  const { userId, userName, displayName, picture } = user
+  const [status, setStatus] = useState("waiting...")
+  //hook to get the user's status from the server
+  useEffect(()=> {
+	setStatus(getUserStatus(userId).body.status)
+  }, [userId])
+
   return (
     <div id="conversation_info_row" className="row chat-row">
       <div className="col-2 profile-container">
-        <img alt="img" src="pictures/face2.jpg" />
+        <img alt="img" src={picture} />
       </div>
       <div className="col-10 d-flex">
         <div className="row name-time-last-message-container">
-          <div className="col-12 contact_name fw-bold">
-            name another name and one more and even one more
+          <div className="col-12 contact_name fw-bold">{displayName}</div>
+          <div className="col-12 last_msg fw-lighter fst-italic">
+            {status}
           </div>
-          <div className="col-12 last_msg fw-lighter fst-italic">online</div>
         </div>
       </div>
     </div>
