ac7dedf1ba1e04ca72bd33e8da9b81240bcb3c0e
dstr951 <dstr159@gmail.com>
2023-05-10 04:47:42 +0300

    add parameters to OpenChar component

--- client-side/src/components/chat/OpenChat.js
+++ client-side/src/components/chat/OpenChat.js
@@ -1,11 +1,11 @@
 import OpenChatContactRow from "./OpenChatContactRow";
 import OpenChatMessages from "./OpenChatMessages";
 
-export default function OpenChat() {
+export default function OpenChat({ user, chatId, myId }) {
   return (
     <>
-      <OpenChatContactRow />
-      <OpenChatMessages messages = {[{content: "aaa", timeSent: new Date(), senderId: 1}, {content: "aaa", timeSent: new Date(), senderId: 2}]} myId = {1}/>
+      <OpenChatContactRow user={user} />
+      <OpenChatMessages chatId={chatId} myId={myId} />
       <div id="chat_input" className="chat_row row ms-0 me-0">
         <input id="input_message" className="col-10" />
         <button className="col-2">send</button>
