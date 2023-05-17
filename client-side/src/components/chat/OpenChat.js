ae99189e14681d89241d9e29b41f3b2060c86c1d
dstr951 <dstr159@gmail.com>
2023-05-17 13:12:44 +0300

    add send message functionality to page

--- client-side/src/components/chat/OpenChat.js
+++ client-side/src/components/chat/OpenChat.js
@@ -1,15 +1,43 @@
-import OpenChatContactRow from "./OpenChatContactRow";
-import OpenChatMessages from "./OpenChatMessages";
+import OpenChatContactRow from "./OpenChatContactRow"
+import OpenChatMessages from "./OpenChatMessages"
 
-export default function OpenChat({ user, chatId, myId }) {
+import {useState} from "react"
+import {sendMessage} from "../../apiTemp"
+
+export default function OpenChat({user, chatId, myId}) {
+  const [messageContent, setMessageContent] = useState("")
+  const [forceUpadteMessages, setForceUpdateMessages] = useState(false)
+
+  function inputChange(e) {
+    setMessageContent(e.target.value)
+  }
+
+  function sendMessageHandler(e) {
+    e.preventDefault()
+    sendMessage(messageContent, myId, chatId)
+    setForceUpdateMessages((value) => !value)
+    setMessageContent("")
+  }
+
   return (
     <>
       <OpenChatContactRow user={user} />
-      <OpenChatMessages chatId={chatId} myId={myId} />
+      <OpenChatMessages
+        chatId={chatId}
+        myId={myId}
+        forceUpadteMessages={forceUpadteMessages}
+      />
       <div id="chat_input" className="chat_row row ms-0 me-0">
-        <input id="input_message" className="col-10" />
-        <button className="col-2">send</button>
+        <input
+          id="input_message"
+          className="col-10"
+          value={messageContent}
+          onChange={inputChange}
+        />
+        <button className="col-2" onClick={sendMessageHandler}>
+          send
+        </button>
       </div>
     </>
-  );
+  )
 }
