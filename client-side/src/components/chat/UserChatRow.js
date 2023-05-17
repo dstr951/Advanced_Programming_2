a57cbe56afdf9598c6ef0b9d2858df589241491f
dstr951 <dstr159@gmail.com>
2023-05-17 23:52:11 +0300

    change title

--- client-side/src/components/chat/UserChatRow.js
+++ client-side/src/components/chat/UserChatRow.js
@@ -70,7 +70,7 @@ export default function UserChatRow({ myId, setForceUpdateMessages }) {
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title">Modal title</h5>
+                  <h5 className="modal-title">Add Chat</h5>
                   <button
                     type="button"
                     className="btn-close"
