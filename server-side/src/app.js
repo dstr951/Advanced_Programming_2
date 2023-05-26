b9a9d8dc066653f182baac9523955cd691a9449e
tomerKimberg <tomerkimberg@gmail.com>
2023-05-26 12:34:25 +0300

    import mongoose and create a connection export mongoose

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,1 +1,19 @@
-import express from "express";
+const express = require("express");
+const mongoose = require('mongoose').default;
+const cors = require("cors")
+const app = express()
+
+
+mongoose.connect('mongodb://0.0.0.0:27017/latestDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+
+module.exports = {mongoose}
