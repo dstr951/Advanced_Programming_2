c076e1e01c5411e6bb8919a704d9880879452bb9
tomerKimberg <tomerkimberg@gmail.com>
2023-05-26 12:34:54 +0300

    create schema for User and UserPassName

--- server-side/src/models/Users.js
+++ server-side/src/models/Users.js
@@ -1,3 +1,49 @@
-import express from "express";
-
+const {mongoose} = require('../app')
 //todo here-  create scheme for UserPassName, User (register user).
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    username:{
+        type:String,
+        unique: true,
+        default: null
+
+    },
+    displayName:{
+        type:String,
+        default: null
+    },
+    profilePic:{
+        type:String,
+        default: null
+    }
+})
+
+const UserPassNameSchema = new Schema({
+    username:{
+        type:String,
+        unique: true,
+        default: null
+
+    },
+    password:{
+        type:String,
+        default: null
+    },
+    displayName:{
+        type:String,
+        default: null
+    },
+    profilePic:{
+        type:String,
+        default: null
+    }
+})
+
+
+
+const User = mongoose.model('User', UserSchema)
+const UserPassName = mongoose.model('UserPassName', UserPassNameSchema)
+
+module.exports = {User:User, UserPassName:UserPassName}
