cc06c87ef953e053db022621bb9b1fa0222948e9
tomerKimberg <tomerkimberg@gmail.com>
2023-05-28 13:05:05 +0300

    added contorller for Users.js. logic works, need to add the res part

--- server-side/src/controllers/Users.js
+++ server-side/src/controllers/Users.js
@@ -1,1 +1,23 @@
-import express from "express";
+const UserService  = require("../services/Users");
+
+
+
+
+async function registerUser(req,res){
+    //res.json( await userService.registerUser(req.username, req.password, req.displayName, req.profilePic))
+    const toRes = await UserService.registerUser(
+        {username:req.username,
+            password:req.password,
+            displayName:req.displayName,
+            profilePic:req.profilePic})
+    console.log(toRes)
+}
+
+async function getUserInfo(req,res){
+    //res.json( await userService.registerUser(req.username, req.password, req.displayName, req.profilePic))
+    const toRes = await UserService.getUserInfo(req.username)
+    console.log(toRes)
+}
+
+registerUser({username:"tomer5",password:"Asdf1234",displayName:"tomer5", profilePic:"base64"},{})
+getUserInfo({username:"tomer1"},{})
