af6aa5d71b42bb5e4275aae99e8c94894f78b8fa
dstr951 <dstr159@gmail.com>
2023-06-01 09:42:53 +0300

    send text response in /Tokens

--- server-side/src/controllers/Tokens.js
+++ server-side/src/controllers/Tokens.js
@@ -4,7 +4,7 @@ const TokenService = require("../services/Tokens")
 async function processLogin(req,res){
     const response = TokenService.processLogin(req.body.username, req.body.password)
     if((await response).status === 200){
-        res.status(200).json((await response).body)
+        res.status(200).send((await response).body)
     }
     else if((await response).status === 400){
         res.status((await response).status).send((await response).body)
