901d202df045457ce24a398dbf1956febf7c9a02
tomerKimberg <tomerkimberg@gmail.com>
2023-06-11 12:38:08 +0300

    Packages: adapters- all the adapters convert java objects to xml view. api- all requests to server. entities- all local DB tables. repositories- sync local DB and make server requests. viewmodels- get data from repository. activities- UI what the user sees.

---
+++ android/app/src/main/java/com/example/foochat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.foochat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
