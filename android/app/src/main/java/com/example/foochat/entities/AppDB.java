dea630900776f54d2d42a0d8c92ecf308750aa62
tomerKimberg <tomerkimberg@gmail.com>
2023-06-13 11:54:21 +0300

    added abstract classes of the dao's

--- android/app/src/main/java/com/example/foochat/entities/AppDB.java
+++ android/app/src/main/java/com/example/foochat/entities/AppDB.java
@@ -4,7 +4,11 @@ package com.example.foochat.entities;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {UserTable.class}, version = 1)
+@Database(entities = {UserTable.class, PersonTable.class, ChatsTable.class, MessagesTable.class}, version = 1)
 public abstract class AppDB extends RoomDatabase{
     public abstract UserDao userDao();
+    public abstract PersonDao personDao();
+    public abstract ChatsDao chatsDao();
+    public abstract MessagesDao messagesDao();
+
 }
