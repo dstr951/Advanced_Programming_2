16d1bdc2185eb56f607a9a5071b7db3dedc2b1a6
tomerKimberg <tomerkimberg@gmail.com>
2023-06-14 17:54:46 +0300

    started working on the repo

--- android/app/src/main/java/com/example/foochat/repositories/ChatsRepo.java
+++ android/app/src/main/java/com/example/foochat/repositories/ChatsRepo.java
@@ -1,4 +1,33 @@
 package com.example.foochat.repositories;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.foochat.api.ChatsApi;
+import com.example.foochat.entities.AppDB;
+import com.example.foochat.entities.ChatsDao;
+import com.example.foochat.entities.ChatsTable;
+import com.example.foochat.entities.PersonTable;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class ChatsRepo {
+    private MutableLiveData<List<ChatsTable>> chats;
+    private MutableLiveData<List<PersonTable>> persons;
+
+    private ChatsApi chatsApi;
+    private ChatsDao chatsDao;
+
+    public ChatsRepo(AppDB db) {
+        chats = new MutableLiveData<>();
+        chats.setValue(new LinkedList<>());
+        persons = new MutableLiveData<>();
+        persons.setValue(new LinkedList<>());
+        chatsDao = db.chatsDao();
+        chatsApi = new ChatsApi(chatsDao);
+    }
+
+
+
+
 }
