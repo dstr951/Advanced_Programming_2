d28fcc79605c65261bd4a29843750a4667d0eafc
tomerKimberg <tomerkimberg@gmail.com>
2023-06-15 10:11:39 +0300

    requestObjects

---
+++ android/app/src/main/java/com/example/foochat/requestObjects/LoginUserReq.java
@@ -0,0 +1,27 @@
+package com.example.foochat.requestObjects;
+
+public class LoginUserReq {
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LoginUserReq(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
